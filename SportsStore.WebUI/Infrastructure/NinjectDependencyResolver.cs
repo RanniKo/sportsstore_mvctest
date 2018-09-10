4bff39827fce6b70febb33337d3c2dc22ea6b138
RanniKo <ranniko@naver.com>
2018-09-10 18:57:24 +0900

    sport store project layer commit

---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel Kernel;
+
+        public NinjectDependencyResolver(IKernel pKernel)
+        {
+            Kernel = pKernel;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            //binding
+        }
+
+        public object GetService(Type pServiceType)
+        {
+            return Kernel.TryGet(pServiceType);
+        }
+
+        public IEnumerable<object> GetServices(Type pServiceType)
+        {
+            return Kernel.GetAll(pServiceType);
+        }
+    }
+}
